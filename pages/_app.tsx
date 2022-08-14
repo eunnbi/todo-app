28817fb0f3a4721c003035aba85bfe43a5effc0c
Eunbi Kang <jenabill@naver.com>
2022-08-14 21:31:13 +0900

    Refactor: MyApp 컴포넌트에 RecoilRoot, ThemeStyle, GlobalStyle 렌더링

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import ThemeStyle from "../styles/ThemeStyle";
+import GlobalStyle from "../styles/GlobalStyle";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <ThemeStyle>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeStyle>
+    </RecoilRoot>
+  );
 }
 
-export default MyApp
+export default MyApp;
