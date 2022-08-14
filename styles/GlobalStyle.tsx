264ceb866d32bf210c973edb666ddaeb322629bc
Eunbi Kang <jenabill@naver.com>
2022-08-15 00:33:55 +0900

    Refactor: 스크롤바 글로벌 스타일 추가

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -10,6 +10,23 @@ const GlobalStyle = () => {
           background-color: ${theme.color.bgColor};
           transition: all 0.2s ease-in-out;
         }
+        input {
+          color: ${theme.color.textColor};
+        }
+        ::-webkit-scrollbar {
+          width: 5px; /* 세로축 스크롤바 길이 */
+          height: 5px; /* 가로축 스크롤바 길이 */
+        }
+        ::-webkit-scrollbar-track {
+          background-color: ${theme.color.boxColor};
+        }
+        ::-webkit-scrollbar-track-piece {
+          background-color: ${theme.color.boxColor};
+        }
+        ::-webkit-scrollbar-thumb {
+          border-radius: 8px;
+          background-color: lightgray;
+        }
       `}
     />
   );
