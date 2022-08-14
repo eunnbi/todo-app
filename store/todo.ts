449d0d421e77bbdbb74cb6ab52765f71709d653b
Eunbi Kang <jenabill@naver.com>
2022-08-15 00:32:58 +0900

    Refactor: interface명을 TodoState로 수정

--- store/todo.ts
+++ store/todo.ts
@@ -1,12 +1,12 @@
 import { atom } from "recoil";
 
-export interface TodoItem {
+export interface TodoState {
   id: number;
   text: string;
   done: boolean;
 }
 
-export const todoListState = atom<TodoItem[]>({
+export const todoListState = atom<TodoState[]>({
   key: "todoListState",
   default: [],
 });
