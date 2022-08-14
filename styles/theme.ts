db5db55af31f64488bc4cad393b6bf32471275e4
Eunbi Kang <jenabill@naver.com>
2022-08-14 21:27:59 +0900

    Add: lightTheme과 darkTheme 추가

---
+++ styles/theme.ts
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  color: {
+    bgColor: "#f0efea",
+    boxColor: "#fff",
+    textColor: "#000",
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+  },
+  name: "light" as const,
+};
+
+export const darkTheme = {
+  color: {
+    bgColor: "#000",
+    boxColor: "#252525",
+    textColor: "#fff",
+    shadowColor: "#000",
+  },
+  name: "dark" as const,
+};
