cf1d3964ace38e50d00ddb3a99ac65046bae9fef
Eunbi Kang <jenabill@naver.com>
2022-08-15 11:06:42 +0900

    Refactor: todoListStatsState selector 추가

--- store/todo.ts
+++ store/todo.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface TodoState {
   id: number;
@@ -9,4 +9,21 @@ export interface TodoState {
 export const todoListState = atom<TodoState[]>({
   key: "todoListState",
   default: [],
+});
+
+export const todoListStatsState = selector({
+  key: "todoListStatsState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalDoneNum = todoList.filter((todo) => todo.done).length;
+    const percentage =
+      totalNum === 0 ? 0 : Number(((totalDoneNum / totalNum) * 100).toFixed(0));
+
+    return {
+      totalNum,
+      totalDoneNum,
+      percentage,
+    };
+  },
 });
