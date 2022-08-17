ad5ba00aaed082ccf1d6bafee2e6bab895b1fd81
Eunbi Kang <jenabill@naver.com>
2022-08-17 18:18:16 +0900

    Refactor: mui의 ThemeProvider 추가

--- styles/ThemeStyle.tsx
+++ styles/ThemeStyle.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from "@emotion/react";
+import { createTheme, ThemeProvider as MuiThemeProvider } from "@mui/material";
 import React from "react";
 import { useRecoilValue } from "recoil";
 import { isLightThemeState } from "../store/theme";
-import { darkTheme, lightTheme } from "./theme";
+import { darkTheme, lightTheme, muiTheme } from "./theme";
 
 const ThemeStyle = ({ children }: React.PropsWithChildren) => {
   const isLightTheme = useRecoilValue(isLightThemeState);
   return (
-    <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
-      {children}
-    </ThemeProvider>
+    <MuiThemeProvider theme={muiTheme}>
+      <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
+        {children}
+      </ThemeProvider>
+    </MuiThemeProvider>
   );
 };
 
