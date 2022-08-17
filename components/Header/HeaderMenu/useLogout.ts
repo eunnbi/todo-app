cd8a0676bba316dc856b594f774020463f1e5cca
Eunbi Kang <jenabill@naver.com>
2022-08-17 18:39:07 +0900

    Refactor: onLogout 함수에서 새로고침 로직 제거

--- components/Header/HeaderMenu/useLogout.ts
+++ components/Header/HeaderMenu/useLogout.ts
@@ -3,7 +3,6 @@ import { signOut } from "../../../api/auth";
 export const useLogout = () => {
   const onLogout = () => {
     signOut();
-    window.location.reload();
   };
   return onLogout;
 };
