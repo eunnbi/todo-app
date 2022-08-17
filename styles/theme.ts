46af2d4ce807196ef11f6f0bea650bd15f952910
Eunbi Kang <jenabill@naver.com>
2022-08-17 18:17:50 +0900

    Refactor: mui theme 커스텀 객체 추가

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,3 +1,5 @@
+import { createTheme } from "@mui/material";
+
 const deviceSizes = {
   tablet: "900px",
   mobile: "600px",
@@ -10,13 +12,18 @@ const device = {
   smallMobile: `screen and (max-width: ${deviceSizes.smallMobile})`,
 };
 
+const mainColor = {
+  light: "#45bf8b",
+  dark: "#64E6AC",
+};
+
 export const lightTheme = {
   color: {
     bgColor: "#f2f2f2",
     boxColor: "#fff",
     textColor: "#000",
     shadowColor: "rgba(0, 0, 0, 0.2)",
-    mainColor: "#64E6AC",
+    mainColor: mainColor.light,
   },
   name: "light" as const,
   device,
@@ -28,8 +35,20 @@ export const darkTheme = {
     boxColor: "#383838",
     textColor: "#fff",
     shadowColor: "#000",
-    mainColor: "#45bf8b",
+    mainColor: mainColor.dark,
   },
   name: "dark" as const,
   device,
 };
+
+export const muiTheme = createTheme({
+  ...createTheme(),
+  palette: {
+    primary: {
+      main: mainColor.light,
+    },
+    secondary: {
+      main: mainColor.dark,
+    },
+  },
+});
