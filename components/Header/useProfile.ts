b58813e56c4ec090f08767209dc7e3ef0140f115
Eunbi Kang <jenabill@naver.com>
2022-08-17 18:22:25 +0900

    Add: useProfile hook

---
+++ components/Header/useProfile.ts
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { getProfile } from "../../api/profile";
+import { useRecoilState } from "recoil";
+import { profileState } from "../../store/profile";
+
+export const useProfile = (
+  id: string | undefined,
+  email: string | undefined
+) => {
+  const [profile, setProfile] = useRecoilState(profileState);
+  const getUserProfile = async (id: string) => {
+    const { data, error } = await getProfile({ id });
+    if (error) {
+      if (email === undefined) return;
+      setProfile({
+        id,
+        username: email.split("@")[0],
+      });
+    } else {
+      setProfile(data);
+    }
+  };
+  useEffect(() => {
+    if (id !== undefined) {
+      getUserProfile(id);
+    }
+  }, [id]);
+  return { profile };
+};
