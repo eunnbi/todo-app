a3f8cfb1e2c2b39f7dab975d7e45d0640515d828
Eunbi Kang <jenabill@naver.com>
2022-08-17 18:39:32 +0900

    Refactor: _app.tsx에 Layout 컴포넌트 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,16 @@ import type { AppProps } from "next/app";
 import ThemeStyle from "../styles/ThemeStyle";
 import GlobalStyle from "../styles/GlobalStyle";
 import { RecoilRoot } from "recoil";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ThemeStyle>
         <GlobalStyle />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeStyle>
     </RecoilRoot>
   );
