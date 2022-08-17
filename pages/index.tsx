fe1414ef4ec8fa02775400db41a84f4b821e8b79
Eunbi Kang <jenabill@naver.com>
2022-08-17 18:39:51 +0900

    Refactor: Home page 렌더링 로직 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from "next";
 import styled from "@emotion/styled";
 import CustomHead from "../components/common/CustomHead";
-import TodoTemplate from "../components/TodoTemplate";
+import TodoTemplate from "../components/todo/TodoTemplate";
+import { useSession } from "../hooks/useSession";
+import LoginForm from "../components/login/LoginForm";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
+  const { session } = useSession();
   return (
     <>
       <CustomHead />
       <Wrapper>
-        <TodoTemplate />
+        {session === null ? (
+          <LoginForm />
+        ) : (
+          <>
+            <Header />
+            <TodoTemplate />
+          </>
+        )}
       </Wrapper>
     </>
   );
@@ -18,7 +29,9 @@ export default Home;
 
 const Wrapper = styled.div`
   height: 100vh;
+  max-width: 500px;
+  width: 90vw;
+  margin: 0 auto;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
 `;
